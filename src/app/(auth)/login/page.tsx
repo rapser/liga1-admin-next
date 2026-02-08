a01e1d70f943f21f2d61db5bb7c8ae8d50374281
Miguel Angel Tomairo Mendez <matomairo@gmail.com>
2026-02-07 22:54:15 -0500

    feat: hide button login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -213,8 +213,8 @@ export default function LoginPage() {
                     )}
                   </Button>
 
-                  {/* Divider */}
-                  <div className="relative my-6">
+                  {/* TODO: Descomentar para habilitar login con Google */}
+                  {/* <div className="relative my-6">
                     <div className="absolute inset-0 flex items-center">
                       <div className="w-full border-t border-[#dee2e6]"></div>
                     </div>
@@ -223,7 +223,6 @@ export default function LoginPage() {
                     </div>
                   </div>
 
-                  {/* Google Sign In Button */}
                   <Button
                     type="button"
                     onClick={handleGoogleLogin}
@@ -242,7 +241,7 @@ export default function LoginPage() {
                       <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
                     </svg>
                     Iniciar sesión con Google
-                  </Button>
+                  </Button> */}
 
                   {/* Info Text */}
                   <div className="text-center space-y-2 pt-4">
